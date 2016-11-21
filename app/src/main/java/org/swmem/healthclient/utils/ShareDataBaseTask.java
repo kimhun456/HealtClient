0097337b7ecc0c4840cf1a3784111b5b8070257a
hyunjae <kimhun456@gmail.com>
2016-11-21 17:57:44 +0900

    Dialog추가
    db file -> csv파일로 전환

--- app/src/main/java/org/swmem/healthclient/utils/ShareDataBaseTask.java
+++ app/src/main/java/org/swmem/healthclient/utils/ShareDataBaseTask.java
@@ -3,14 +3,25 @@ package org.swmem.healthclient.utils;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v4.BuildConfig;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
+
+import org.swmem.healthclient.db.HealthContract;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.nio.channels.FileChannel;
 
 import static android.os.Environment.getDataDirectory;
@@ -28,6 +39,7 @@ public class ShareDataBaseTask extends AsyncTask<Void,Void,Void> {
 
 
     private String backupDBPath = "backup.db";
+    private String csvPath = "glucare.csv";
     private Context context;
     public ShareDataBaseTask(Context context){
         this.context = context.getApplicationContext();
@@ -41,15 +53,21 @@ public class ShareDataBaseTask extends AsyncTask<Void,Void,Void> {
         intent.setType("text/plain");
 
         File root = getExternalStorageDirectory();
-        File file = new File(root, backupDBPath);
+        File file = new File(root, csvPath);
         if (!file.exists() || !file.canRead()) {
             Toast.makeText(context, "Attachment Error", Toast.LENGTH_SHORT).show();
             return;
         }
-        Uri uri = Uri.fromFile(file);
+
+        Uri uri = FileProvider.getUriForFile(context,
+                "org.swmem.healthclient.provider",
+                file);
+
         intent.putExtra(Intent.EXTRA_STREAM, uri);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        Intent openInChooser = Intent.createChooser(intent, "Share with...");
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,openInChooser,0);
         try {
             pendingIntent.send();
         } catch (PendingIntent.CanceledException e) {
@@ -60,27 +78,73 @@ public class ShareDataBaseTask extends AsyncTask<Void,Void,Void> {
     public void exportDatabase(String databaseName) {
         try {
             File sd = getExternalStorageDirectory();
-            File data = getDataDirectory();
+
 
             if (sd.canWrite()) {
-                String currentDBPath = "//data//"+context.getPackageName()+"//databases//"+databaseName+"";
-                File currentDB = new File(data, currentDBPath);
-                File backupDB = new File(sd, backupDBPath);
 
-                if (currentDB.exists()) {
-                    FileChannel src = new FileInputStream(currentDB).getChannel();
-                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
-                    dst.transferFrom(src, 0, src.size());
-                    src.close();
-                    dst.close();
+                String[] selectionArgs = {""};
+
+                Cursor cursor = context.getContentResolver().query(
+                        HealthContract.GlucoseEntry.CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        null
+                );
+
+                Log.v ("cursor" ,"se");
+
+                File exportDir = new File(Environment.getExternalStorageDirectory(), "");
+                if (!exportDir.exists())
+                {
+                    exportDir.mkdirs();
                 }
+
+                File file = new File(exportDir, csvPath);
+                try
+                {
+                    file.createNewFile();
+                    CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+//                    Log.v ("cursor" ,"date : " + cursor.getColumnNames());
+                    csvWrite.writeNext(cursor.getColumnNames());
+                    while(cursor.moveToNext())
+                    {
+                        //Which column you want to exprort
+                        String arrStr[] ={
+                                cursor.getString(0),
+                                cursor.getString(1),
+                                cursor.getString(2),
+                                cursor.getString(3),
+                                cursor.getString(4),
+                                cursor.getString(5),
+                                cursor.getString(6)
+
+                        };
+
+                        Log.v("cursor : ", "" + cursor.getString(1));
+
+                        csvWrite.writeNext(arrStr);
+                    }
+                    csvWrite.close();
+                    cursor.close();
+                }
+                catch(Exception sqlEx)
+                {
+                    Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+                }
+//
+//                if (currentDB.exists()) {
+//                    FileChannel src = new FileInputStream(currentDB).getChannel();
+//                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+//                    dst.transferFrom(src, 0, src.size());
+//                    src.close();
+//                    dst.close();
+//                }
             }
         } catch (Exception e) {
             Log.e("Database Export",e.toString());
         }
     }
-
-
 
 
     @Override
